bb0ce207f52dda50efe18994b3808c74f9695b6a
Giorgi Dogadze <g.dogadze@gmail.com>
2024-11-21 12:30:40 +0400

    fix: design

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -3,17 +3,18 @@ import Slider from "react-slick";
 import image1 from "../images/image_1.png";
 import image2 from "../images/image_2.png";
 import image3 from "../images/image_3.png";
-import image4 from "../images/image_4.png";
+import image4 from "../images/image_4.jpg";
 import image5 from "../images/image_5.png";
 import image6 from "../images/image_6.png";
 import image7 from "../images/image_7.png";
 import image8 from "../images/image_8.png";
 import image9 from "../images/image_9.png";
 import image10 from "../images/image_10.png";
-import image11 from "../images/image_11.png";
+import image11 from "../images/image_11.jpg";
 import image12 from "../images/image_12.png";
 import image13 from "../images/image_13.png";
 import image14 from "../images/image_14.png";
+import image15 from "../images/image_15.png";
 import "../App.css"; // Add your global styling for the app if needed
 
 const SampleNextArrow = (props: any) => {
@@ -66,6 +67,7 @@ const ImageSlider = () => {
     image12,
     image13,
     image14,
+    image15
   ];
   const settings = {
     dots: true,
